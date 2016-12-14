afae8a2fc34f7a5d97fb1883b6b3f3927690d7f5
Stan Bartlett <stanbartlett@gmail.com>
2016-12-13 20:53:54 -0500

    finally got back to this. fixed s1c6 - had to try more initial blocks to get best keysize.

--- set1/s1c6_break_rep_key_xor.py
+++ set1/s1c6_break_rep_key_xor.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 import binascii
 import base64
+import itertools
 from hamming_dist import hamming_dist, str_2_bin
 from s1c4_detect_single_char_xor import read_file
 from s1c5_rep_key_xor import rep_xor
 from s1c2_fixed_xor import fixed_xor
 from s1c3_crack_single_byte_xor import hax_the_gibson
-
-
-def get_first_byte_blocks(in_bytes, keysize):
-   block1 = in_bytes[:keysize].decode('utf-8')
-   block2 = in_bytes[keysize:(2 * keysize)].decode('utf-8')
-   return (block1, block2)
 
 
 def get_byte_blocks(in_bytes, keysize):
@@ -24,14 +19,11 @@ def get_byte_blocks(in_bytes, keysize):
 
 def find_best_keysize(in_bytes, lb=2, ub=40, scorer=hamming_dist):
     ub = ub + 1
-    # res = {k: scorer(*next(get_byte_blocks(in_bytes, k))) / k for k in range(lb, ub)}
-    res = {k: scorer(*get_first_byte_blocks(in_bytes, k)) / k for k in range(lb, ub)}
-
-    ###
-    for asdf in res.items():
-        print(asdf)
-    ###
-
+    res = {}
+    for k in range(lb, ub):
+        blocks = get_byte_blocks(in_bytes, k)
+        pairs = list(itertools.combinations(blocks, 2))[:10]
+        res[k] = sum((scorer(*pair) / k for pair in pairs)) / 10
     return min(res, key=res.get)
 
 
@@ -42,6 +34,7 @@ def find_key(blocks, keysize):
         key += bytes([hax_the_gibson(k_bytes)[0]])
     return key
 
+
 if __name__ == '__main__':
     fp = './6.txt'
 
@@ -50,6 +43,8 @@ if __name__ == '__main__':
     keysize = find_best_keysize(in_bytes)
     blocks = get_byte_blocks(in_bytes, keysize)
     key = find_key(blocks, keysize)
-    print(key, type(key)) # byte object of key
-    print(binascii.hexlify(key)) # hex of hte key
-    print(rep_xor(in_bytes, key)) # returns byte object
+    print('Key is {}'.format(key)) # byte object of key
+
+    message = bytes.fromhex(rep_xor(in_bytes, key).decode())
+    print('Message: {}'.format(message))
+
